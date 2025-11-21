5505014ff020b6d735c5e0c12326dca7dee8fbb2
Natnael Ayele <n.eticha@alustudent.com>
2025-11-21 17:50:23 +0200

    fixed printing methode for better readability

--- officer.py
+++ officer.py
@@ -14,7 +14,6 @@ def officer_login():
     password = input("Password: ").strip()
     if username in officers and officers[username] == password:
         print(f"Welcome Officer {username}!")
-        officer_menu(username)
         global current_officer
         global officer_id
         current_officer = username 
@@ -54,16 +53,16 @@ def officer_menu(username):
 def view_all_cases():
     conn = init_db()
     cursor = conn.cursor()
-    cursor.execute("SELECT case_id, first_name, last_name, age, gender, location, abuse_type,case_status, date_reported FROM cases")
+    cursor.execute("SELECT case_id, first_name, last_name, age, gender, location, abuse_type,case_status, date_reported,status_updated_by FROM cases")
     rows = cursor.fetchall()
 
     if rows:
         print("\n--- All Cases ---")
         for row in rows:
-                case_id, first_name, last_name, age, gender, location, abuse_type,case_status, date_reported = row
+                case_id, first_name, last_name, age, gender, location, abuse_type,case_status, date_reported, status_updated_by = row
                 print(f"ID:{case_id} | {first_name} {last_name} | Age:{age} | Gender:{gender} | "
                   f"Location:{location} | Abuse:{abuse_type} |"
-                  f"Status:{case_status} | Reported:{date_reported} |")
+                  f"Status:{case_status} | Reported:{date_reported} | Status updated by: {status_updated_by}")
     else:
         print("No cases found.")
 
@@ -73,11 +72,24 @@ def search_case():
     conn = init_db()
     cursor = conn.cursor()
     case_id = input ("Enter ID: ")
-    cursor.execute("SELECT * FROM cases WHERE case_id=%s", (case_id,))
+    cursor.execute("SELECT case_id, first_name, last_name, age, location, abuse_type, status, date_reported, follow_up_note, status_updated_by  FROM cases WHERE case_id=%s", (case_id,))
     row = cursor.fetchone()
 
     if row:
-        print(row)
+        case_id, first_name, last_name, age, location, abuse_type, status, date_reported, follow_up_note, status_updated_by = row
+        print("\n------ CASE DETAILS ------")
+        print(f"Case ID     : {case_id}")
+        print(f"First Name  : {first_name}")
+        print(f"Last Name   : {last_name}")
+        print(f"Age         : {age}")
+        print(f"Location    : {location}")
+        print(f"Abuse Type  : {abuse_type}")
+        print(f"Status      : {status}")
+        print(f"Submitted on  : {date_reported}")
+        print(f"Status      : {status}")
+        print(f"note     : {follow_up_note}")
+        print(f"Status updated by     : {status_updated_by}")
+        print("---------------------")
     else:
         print("Case not found.")
     
@@ -90,12 +102,24 @@ def filter_cases():
     cursor = conn.cursor()
     status = input("Enter Status: ").strip()
     
-    cursor.execute("SELECT case_id, first_name, last_name, age, gender, location, abuse_type,case_status, date_reported FROM cases WHERE case_status=%s", (status,))
+    cursor.execute("SELECT case_id, first_name, last_name, age, gender, location, abuse_type,case_status, date_reported,status_updated_by FROM cases WHERE case_status=%s", (status,))
     rows = cursor.fetchall()
 
     if rows:
+        case_id, first_name, last_name, age, location, abuse_type, status, date_reported, status_updated_by = row
         for row in rows:
-            print(row)
+
+            print("\n------ CASE DETAILS ------")
+            print(f"Case ID     : {case_id}")
+            print(f"First Name  : {first_name}")
+            print(f"Last Name   : {last_name}")
+            print(f"Age         : {age}")
+            print(f"Location    : {location}")
+            print(f"Abuse Type  : {abuse_type}")
+            print(f"Status      : {status}")
+            print(f"Submitted on  : {date_reported}")
+            print(f"Status updated by     : {status_updated_by}")
+
     else:
         print("No cases found.")
 
@@ -123,7 +147,7 @@ def update_case_status():
     query = """
         UPDATE cases
         SET case_status = %s,
-            follow_up_note = CONCAT(IFNULL(follow_up, ''), ' | ', %s),
+            follow_up_note = CONCAT(IFNULL(follow_up_note, ''), ' | ', %s),
             status_updated_by = %s
         WHERE case_id = %s
     """
