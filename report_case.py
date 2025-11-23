fcf533bd17c7a71f8f827b0c36d2b72b064c62fd
Natnael Ayele <n.eticha@alustudent.com>
2025-11-23 18:01:08 +0200

    removed masking when listing cases

--- report_case.py
+++ report_case.py
@@ -138,7 +138,7 @@ def check_case_status():
 
         for i, r in enumerate(rows, start=1):
             cid,fn, ln, age, loc = r
-            print(f"{i}. Name: {fn[:3]}*** {ln[:3]}*** | Age: {age} | Location: {loc[:4]}***")
+            print(f"{i}. Name: {fn} {ln} | Age: {age} | Location: {loc}")
 
         selection = input("\nWhich one is your case? Enter number: ").strip()
 
